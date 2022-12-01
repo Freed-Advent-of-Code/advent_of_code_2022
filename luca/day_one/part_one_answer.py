8b2c70f3bd31e5c6a7bc0136d8b132f63084560a
hayongwoon <luca.ha@freedgrouptech.com>
2022-12-01 21:54:29 +0900

    add answer

---
+++ luca/day_one/part_one_answer.py
@@ -0,0 +1,25 @@
+class TotalCaloriesIsThatElfCarrying:
+    def __init__(self, file_path) -> None:
+        self.one_elf_total_calories = 0
+        self.max_elf_total_calories = 0
+        self.max_elf_number = 1
+        self.file_path = file_path
+
+    def read_file(self):    
+        with open(self.file_path, 'r') as f:
+            return f.read().split('\n')
+
+    def generate_total_calories_and_elf_number(self):
+        for i in self.read_file():
+            if i == '':
+                self.max_elf_total_calories = max(self.one_elf_total_calories, self.max_elf_total_calories)
+                self.one_elf_total_calories = 0
+                self.max_elf_number += 1
+            else:
+                self.one_elf_total_calories += int(i)
+
+        return f'Max Caloires is {self.max_elf_total_calories}, Elf Number is {self.max_elf_number}'
+
+if __name__ == '__main__':
+    answer = TotalCaloriesIsThatElfCarrying('luca/day_one/input.txt')
+    print(answer.generate_total_calories_and_elf_number())
