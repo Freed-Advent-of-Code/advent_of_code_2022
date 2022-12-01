1a46cb1802138639839227320f198e1db9d9346e
bluewood <bluewood.truth@gmail.com>
2022-12-01 22:24:20 +0900

    remove try/catch

--- hans/src/index.js
+++ hans/src/index.js
@@ -3,20 +3,17 @@ import path from 'path';
 
 async function main(argv) {
   const day = parseInt(argv[2]);
-  try {
-    const moduleName = `day${String(day).padStart(2, '0')}`;
-    const module = await import(`./${moduleName}/index.js`);
 
-    const inputFile = fs.readFileSync(path.resolve('src', moduleName, 'input'));
-    const input = inputFile.toString();
+  const moduleName = `day${String(day).padStart(2, '0')}`;
+  const module = await import(`./${moduleName}/index.js`);
 
-    const startTime = new Date();
-    const result = module.resolve(input);
-    console.log('Puzzle answer is', result);
-    console.log('It takes', `${new Date() - startTime}ms`);
-  } catch (e) {
-    console.error(e);
-  }
+  const inputFile = fs.readFileSync(path.resolve('src', moduleName, 'input'));
+  const input = inputFile.toString();
+
+  const startTime = new Date();
+  const result = module.resolve(input);
+  console.log('Puzzle answer is', result);
+  console.log('It takes', `${new Date() - startTime}ms`);
 }
 
 main(process.argv);
