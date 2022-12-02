8a3fbac4c372dce8e03bc6878bbeaab08554b8ae
Joon Hwan 김준환 <xncbf12@gmail.com>
2022-12-02 20:15:38 +0900

    day02 part two

--- francis/day02/main.py
+++ francis/day02/main.py
@@ -8,5 +8,7 @@ print(part_one())
 def part_two():
     with open('input.txt', 'r') as f:
         data = f.read()
+    return sum(6 if ord(y) - 87 == 3 else 3 if ord(y) - 87 == 2 else 0 for x, y in [line.split(" ") for line in data.split("\n")]) + sum([1,2,3][(ord(x) - (64- (ord(y) - 87))) % 3] for x, y in [line.split(" ") for line in data.split("\n")])
     
+        
 print(part_two())
