615f52dbe3da08467c463c8f92e22ec6705d3b05
bluewood-truth <bluewood.truth@gmail.com>
2022-12-03 00:14:30 +0900

    resolve puzzle 2

--- hans/dart/day02/main.dart
+++ hans/dart/day02/main.dart
@@ -1,6 +1,6 @@
 import '../_lib/read_file.dart';
 
-const scoreMap = {
+const scoreMap1 = {
   'A': {
     'X': 1 + 3,
     'Y': 2 + 6,
@@ -18,15 +18,39 @@ const scoreMap = {
   }
 };
 
-final Map<String, int>? b = scoreMap['A'];
-final c = b?['X'];
+const scoreMap2 = {
+  'A': {
+    'X': 3 + 0,
+    'Y': 1 + 3,
+    'Z': 2 + 6,
+  },
+  'B': {
+    'X': 1 + 0,
+    'Y': 2 + 3,
+    'Z': 3 + 6,
+  },
+  'C': {
+    'X': 2 + 0,
+    'Y': 3 + 3,
+    'Z': 1 + 6,
+  }
+};
 
 void main() {
   final input = readFile('input');
   print(resolvePuzzle1(input));
+  print(resolvePuzzle2(input));
 }
 
 int resolvePuzzle1(String input) {
+  return resolve(input, scoreMap1);
+}
+
+int resolvePuzzle2(String input) {
+    return resolve(input, scoreMap2);
+}
+
+int resolve(String input, Map<String, Map<String, int>> scoreMap) {
   return input
       .split('\n')
       .map((line) => line.split(' '))
