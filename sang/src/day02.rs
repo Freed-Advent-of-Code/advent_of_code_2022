5e7db49690a17aadd0c70a196b750705c94149d8
Wonsang <temporaryaion1@hotmail.com>
2022-12-02 22:42:11 +0900

    solve day 2 part 2

--- sang/src/day02.rs
+++ sang/src/day02.rs
@@ -35,4 +35,30 @@ fn get_score_1(line: &str) -> i32 {
 
 fn get_score_2(line: &str) -> i32 {
     let mut result: i32 = 0;
+    let bytes = line.as_bytes();
+    // A=1, B=2, C=3
+    let elf_hand = bytes[0] as i32 - 'A' as i32 + 1;
+    let my_hand: i32;
+
+    // 0 = loss, 1 = draw, 2 = win
+    let outcome = bytes[2] - 'X' as u8;
+    result += (outcome as i32) * 3;
+
+    if outcome == 1 {
+        my_hand = elf_hand;
+    } else if outcome == 0 {
+        // loss
+        my_hand = (elf_hand + 2) % 3;
+    } else {
+        // win
+        my_hand = (elf_hand + 1) % 3;
+    }
+
+    if my_hand == 0 {
+        result += 3;
+    } else {
+        result += my_hand;
+    }
+
+    result
 }
