56e455f4c6d1e5a90eae97e434368473b55e23ee
Wonsang <temporaryaion1@hotmail.com>
2022-12-03 13:06:49 +0900

    refactor day 2

--- sang/src/day02.rs
+++ sang/src/day02.rs
@@ -17,48 +17,169 @@ pub async fn solve() {
 
 fn get_score_1(line: &str) -> i32 {
     let mut result: i32 = 0;
-    let bytes = line.as_bytes();
 
-    let elf_hand = bytes[0];
-    let my_hand = bytes[2] - 'X' as u8 + 'A' as u8; // A, B, or C
+    let elf_hand = match line.chars().nth(0).unwrap() {
+        'A' => Hand::Rock,
+        'B' => Hand::Paper,
+        _ => Hand::Scissors,
+    };
+    let my_hand = match line.chars().nth(2).unwrap() {
+        'X' => Hand::Rock,
+        'Y' => Hand::Paper,
+        _ => Hand::Scissors,
+    };
 
-    // my hand: A=1, B=2, C=3
-    result += (my_hand + 1 - 'A' as u8) as i32;
-
-    if elf_hand == my_hand {
-        result += 3;
-    } else if elf_hand - 2 == my_hand || elf_hand + 1 == my_hand {
-        result += 6;
-    }
+    result += my_hand.get_value();
+    result += my_hand.plays_against(elf_hand).get_value();
     result
 }
 
 fn get_score_2(line: &str) -> i32 {
     let mut result: i32 = 0;
-    let bytes = line.as_bytes();
-    // A=1, B=2, C=3
-    let elf_hand = bytes[0] as i32 - 'A' as i32 + 1;
-    let my_hand: i32;
+    let elf_hand = match line.chars().nth(0).unwrap() {
+        'A' => Hand::Rock,
+        'B' => Hand::Paper,
+        _ => Hand::Scissors,
+    };
 
-    // 0 = loss, 1 = draw, 2 = win
-    let outcome = bytes[2] - 'X' as u8;
-    result += (outcome as i32) * 3;
-
-    if outcome == 1 {
-        my_hand = elf_hand;
-    } else if outcome == 0 {
-        // loss
-        my_hand = (elf_hand + 2) % 3;
-    } else {
-        // win
-        my_hand = (elf_hand + 1) % 3;
-    }
-
-    if my_hand == 0 {
-        result += 3;
-    } else {
-        result += my_hand;
-    }
-
+    let my_hand = match line.chars().nth(2).unwrap() {
+        'X' => elf_hand.wins_against(),
+        'Y' => elf_hand.draws_against(),
+        _ => elf_hand.loses_against(),
+    };
+    result += my_hand.get_value();
+    result += my_hand.plays_against(elf_hand).get_value();
     result
+}
+
+#[derive(PartialEq)]
+enum Outcome {
+    Win,
+    Draw,
+    Loss,
+}
+#[derive(PartialEq)]
+enum Hand {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+trait Playable {
+    fn loses_against(&self) -> Hand;
+
+    fn wins_against(&self) -> Hand;
+
+    fn draws_against(&self) -> Hand;
+
+    fn plays_against(&self, hand: Hand) -> Outcome;
+}
+
+impl Playable for Hand {
+    fn draws_against(&self) -> Hand {
+        match self {
+            Hand::Rock => Hand::Rock,
+            Hand::Paper => Hand::Paper,
+            Hand::Scissors => Hand::Scissors,
+        }
+    }
+
+    fn wins_against(&self) -> Hand {
+        match self {
+            Hand::Paper => Hand::Rock,
+            Hand::Rock => Hand::Scissors,
+            Hand::Scissors => Hand::Paper,
+        }
+    }
+
+    fn loses_against(&self) -> Hand {
+        match self {
+            Hand::Paper => Hand::Scissors,
+            Hand::Rock => Hand::Paper,
+            Hand::Scissors => Hand::Rock,
+        }
+    }
+
+    fn plays_against(&self, hand: Hand) -> Outcome {
+        if self.draws_against() == hand {
+            return Outcome::Draw;
+        }
+        match self {
+            Hand::Paper => {
+                if matches!(hand, Hand::Scissors) {
+                    Outcome::Loss
+                } else {
+                    Outcome::Win
+                }
+            }
+            Hand::Scissors => {
+                if matches!(hand, Hand::Rock) {
+                    Outcome::Loss
+                } else {
+                    Outcome::Win
+                }
+            }
+            Hand::Rock => {
+                if matches!(hand, Hand::Paper) {
+                    Outcome::Loss
+                } else {
+                    Outcome::Win
+                }
+            }
+        }
+    }
+}
+
+trait Value {
+    fn get_value(&self) -> i32;
+}
+
+impl Value for Hand {
+    fn get_value(&self) -> i32 {
+        match self {
+            Hand::Rock => 1,
+            Hand::Paper => 2,
+            Hand::Scissors => 3,
+        }
+    }
+}
+
+impl Value for Outcome {
+    fn get_value(&self) -> i32 {
+        match self {
+            Outcome::Win => 6,
+            Outcome::Draw => 3,
+            Outcome::Loss => 0,
+        }
+    }
+}
+
+#[test]
+fn test_hand() {
+    let paper = Hand::Paper;
+
+    assert!(paper.loses_against() == Hand::Scissors);
+    assert!(paper.wins_against() == Hand::Rock);
+    assert!(paper.draws_against() == Hand::Paper);
+}
+
+#[test]
+fn test_outcome() {
+    let paper = Hand::Paper;
+
+    assert!(paper.plays_against(Hand::Scissors) == Outcome::Loss);
+    assert!(paper.plays_against(Hand::Paper) == Outcome::Draw);
+    assert!(paper.plays_against(Hand::Rock) == Outcome::Win);
+}
+
+#[test]
+fn test_score_2() {
+    let line = "A Y";
+    assert_eq!(get_score_2(line), 4);
+
+    let line = "B X";
+    assert_eq!(get_score_2(line), 1);
+
+    let line = "C Z";
+    assert_eq!(get_score_2(line), 7);
 }
