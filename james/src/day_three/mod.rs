c51931ec8c952cceed22e3e40a300c38d226f696
jay <dj.min43@gmail.com>
2022-12-03 21:52:59 +0900

    wip: day three

--- james/src/day_three/mod.rs
+++ james/src/day_three/mod.rs
@@ -10,19 +10,6 @@ pub fn compartment_part_one () {
             let v = compartment.to_string();
             let string_length = v.len()/2;
             let (first, second)= v.split_at(string_length);
-            for c in first.split("") {
-                if second.contains(c) {
-                    c.to_string()
-                } else {
-                    String::from("fff")
-                }
-            }
-
-
-            // first.to_string()
-            // first_part
-            // let second_part = &v[*&string_length/2..*&string_length];
-            // first_part
         })
         .collect();
 
