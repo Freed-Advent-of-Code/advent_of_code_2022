0adbcf7dc8e55cc8e3ea76c8dbf38619338471ca
Joon Hwan 김준환 <xncbf12@gmail.com>
2022-12-03 20:37:52 +0900

    part one

--- francis/day03/main.py
+++ francis/day03/main.py
@@ -1,3 +1,4 @@
+import itertools
 def part_one():
     return sum(''.join([chr(x+97) for x in range(26)] + [chr(x+65) for x in range(26)]).find(next(x for x in set(e.strip()[:int(len(e.strip())/2)]) & set(e.strip()[int(len(e.strip())/2):])))+1 for e in open('input.txt', 'r').readlines())
 
