c893b26e65fd6864bc05a2ad4363d8966d492bc4
Dongjoon Min <75886558+djmin43@users.noreply.github.com>
2022-12-04 10:56:26 +0900

    James (#34)
    * day three: part two
    
    * day 3: pt2

--- james/src/main.rs
+++ james/src/main.rs
@@ -1,10 +1,13 @@
 mod day_three;
 
 use day_three::compartment_part_one;
+use crate::day_three::compartment_part_two;
 
 fn main() {
 
     let pt1 = compartment_part_one();
     println!("{}", pt1);
+    let pt2 = compartment_part_two();
+    println!("{}", pt2);
 
 }
