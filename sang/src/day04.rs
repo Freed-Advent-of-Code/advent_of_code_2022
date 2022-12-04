a0f64cadb3ea6fc5722eea90e037b2728ed58612
Wonsang <temporaryaion1@hotmail.com>
2022-12-04 21:28:00 +0900

    solve day4 part 2

--- sang/src/day04.rs
+++ sang/src/day04.rs
@@ -1,18 +1,44 @@
 use crate::helper;
-use std::collections::HashSet;
 
 pub async fn solve() {
     let input = helper::get_input(4).await;
     let part1 = input
         .lines()
         .filter(|line| {
-            let vectors = parse_vectors(line);
-            is_array_contained(&vectors[0], &vectors[1])
+            let pairs = parse_vectors(line);
+            is_array_contained(&pairs[0], &pairs[1])
         })
         .count();
     println!("part 1: {}", part1);
+
+    let part2 = input
+        .lines()
+        .filter(|line| {
+            let pairs = parse_vectors(line);
+            is_array_overlap(&pairs[0], &pairs[1])
+        })
+        .count();
+    println!("part 2: {}", part2);
 }
 
+// part 1
+fn is_array_contained(first: &Vec<i32>, second: &Vec<i32>) -> bool {
+    (is_number_contained(&first, second[0]) && is_number_contained(&first, second[1]))
+        || (is_number_contained(&second, first[0]) && is_number_contained(&second, first[1]))
+}
+
+// part 2
+fn is_array_overlap(first: &Vec<i32>, second: &Vec<i32>) -> bool {
+    (is_number_contained(first, second[0]) || is_number_contained(first, second[1]))
+        || (is_number_contained(second, first[0]) || is_number_contained(second, first[1]))
+}
+
+// common
+fn is_number_contained(arr: &Vec<i32>, num: i32) -> bool {
+    arr[0] <= num && num <= arr[1]
+}
+
+// common
 fn parse_vectors(line: &str) -> Vec<Vec<i32>> {
     line.split(",")
         .map(|vec_str| {
@@ -28,15 +54,6 @@ fn parse_vectors(line: &str) -> Vec<Vec<i32>> {
         .collect()
 }
 
-fn is_array_contained(first: &Vec<i32>, second: &Vec<i32>) -> bool {
-    (is_number_contained(&first, second[0]) && is_number_contained(&first, second[1]))
-        || (is_number_contained(&second, first[0]) && is_number_contained(&second, first[1]))
-}
-
-fn is_number_contained(arr: &Vec<i32>, num: i32) -> bool {
-    arr[0] <= num && num <= arr[1]
-}
-
 #[test]
 fn test_parse_vectors() {
     assert_eq!(parse_vectors("2-4,3-6"), vec![vec![2, 4], vec![3, 6]]);
@@ -49,4 +66,13 @@ fn test_is_contained() {
     assert_eq!(is_array_contained(&vec![2, 4], &vec![6, 6]), false);
     assert_eq!(is_array_contained(&vec![2, 6], &vec![6, 6]), true);
     assert_eq!(is_array_contained(&vec![2, 6], &vec![3, 5]), true);
+}
+
+#[test]
+fn test_is_overlapping() {
+    assert_eq!(is_array_overlap(&vec![2, 4], &vec![3, 6]), true);
+    assert_eq!(is_array_overlap(&vec![2, 4], &vec![4, 4]), true);
+    assert_eq!(is_array_overlap(&vec![7, 7], &vec![2, 9]), true);
+    assert_eq!(is_array_overlap(&vec![2, 4], &vec![5, 6]), false);
+    assert_eq!(is_array_overlap(&vec![2, 2], &vec![5, 6]), false);
 }
