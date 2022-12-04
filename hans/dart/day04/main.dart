2ee14e553ca1790fb813a8ac9721fcd19f6ca9f9
bluewood-truth <bluewood.truth@gmail.com>
2022-12-04 22:31:55 +0900

    resolve puzzle1

--- hans/dart/day04/main.dart
+++ hans/dart/day04/main.dart
@@ -1,5 +1,11 @@
+import '../_lib/measure_time.dart';
 import '../_lib/read_file.dart';
 
 void main() {
-  print("hello world!");
+  final input = readFile('input');
+  print(measureTime(() => resolve(input)));
+}
+
+int resolve(String input) {
+  return input.split('\n').fold( 0, (count, pair) => count + ((List<String> pair) => (int.parse(pair[0].split('-')[0]) <= int.parse(pair[1].split('-')[0]) && int.parse(pair[0].split('-')[1]) >= int.parse(pair[1].split('-')[1])) || (int.parse(pair[1].split('-')[0]) <= int.parse(pair[0].split('-')[0]) && int.parse(pair[1].split('-')[1]) >= int.parse(pair[0].split('-')[1])) ? 1 : 0)(pair.split(',')));
 }
