3a17b5c372f3b82691d3d71c20818bf55f8c2bff
jay <dj.min43@gmail.com>
2022-12-04 09:42:43 +0900

    part one

--- james/src/day_three/mod.rs
+++ james/src/day_three/mod.rs
@@ -1,18 +1,32 @@
+use std::collections::HashMap;
 use std::fs;
 
-pub fn compartment_part_one () {
+pub fn compartment_part_one () -> String {
 
     let file = fs::read_to_string("./src/day_three/input.txt").unwrap();
 
-    let list : Vec<String>  = file
+    let letter_scores = ('a'..='z')
+        .chain('A'..='Z')
+        .enumerate()
+        .map(|(idx, c)| (c, idx + 1))
+        .collect::<HashMap<char, usize>>();
+
+    let list: usize =  file
         .lines()
         .map(|compartment| {
             let v = compartment.to_string();
             let string_length = v.len()/2;
             let (first, second)= v.split_at(string_length);
+            let same_char = first
+                .chars()
+                .find(|c| second.contains(*c))
+                .unwrap();
+            letter_scores.get(&same_char).unwrap()
         })
-        .collect();
+        .sum();
 
-    println!("{:?}", list);
+    list.to_string()
+
+
 
 }
