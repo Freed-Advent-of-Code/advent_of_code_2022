5af8c936e3c7d616b8be6fc083b1c789df63ed34
Wonsang <temporaryaion1@hotmail.com>
2022-12-06 21:45:05 +0900

    solve day 6 part 2

--- sang/src/day06.rs
+++ sang/src/day06.rs
@@ -5,20 +5,23 @@ pub async fn solve() {
     let input = helper::get_input(6).await;
     let part1 = helper::time_function(|| get_part_1(&input));
     println!("part 1: {}", part1);
+
+    let part2 = helper::time_function(|| get_part_2(&input));
+    println!("part 2: {}", part2);
 }
 
-fn get_part_1(input: &str) -> usize {
+fn get_char_len(input: &str, marker_len: usize) -> usize {
     let mut seen: HashMap<char, usize> = HashMap::new();
 
     for (i, ch) in input.chars().enumerate() {
         seen.insert(ch, 1 + if seen.contains_key(&ch) { seen[&ch] } else { 0 });
 
-        if i >= 3 {
-            if seen.keys().count() == 4 {
+        if i >= marker_len - 1 {
+            if seen.keys().count() == marker_len {
                 return i + 1;
             }
 
-            let remove_char = input.chars().nth(i - 3).unwrap();
+            let remove_char = input.chars().nth(i + 1 - marker_len).unwrap();
             if let Some(count) = seen.get(&remove_char) {
                 match count {
                     1 => seen.remove(&remove_char),
@@ -27,7 +30,15 @@ fn get_part_1(input: &str) -> usize {
             }
         }
     }
-    0
+    input.len()
+}
+
+fn get_part_1(input: &str) -> usize {
+    get_char_len(input, 4)
+}
+
+fn get_part_2(input: &str) -> usize {
+    get_char_len(input, 14)
 }
 
 #[test]
@@ -37,4 +48,13 @@ fn test_get_part_1() {
     assert_eq!(get_part_1("nppdvjthqldpwncqszvftbrmjlhg"), 6);
     assert_eq!(get_part_1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 10);
     assert_eq!(get_part_1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 11);
+}
+
+#[test]
+fn test_get_part_2() {
+    assert_eq!(get_part_2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), 19);
+    assert_eq!(get_part_2("bvwbjplbgvbhsrlpgdmjqwftvncz"), 23);
+    assert_eq!(get_part_2("nppdvjthqldpwncqszvftbrmjlhg"), 23);
+    assert_eq!(get_part_2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), 29);
+    assert_eq!(get_part_2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), 26);
 }
