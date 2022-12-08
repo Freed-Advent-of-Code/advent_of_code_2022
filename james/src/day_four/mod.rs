81c3e8a2be127f6fec7dc2fc4159135d5ea26147
jay <dj.min43@gmail.com>
2022-12-08 20:28:34 +0900

    day4: part two

--- james/src/day_four/mod.rs
+++ james/src/day_four/mod.rs
@@ -4,10 +4,31 @@ fn read_file () -> String {
     fs::read_to_string("./src/day_four/input.txt").unwrap()
 }
 
-pub fn pairs_part_one() {
+// pub fn pairs_part_one() {
+//     let file = read_file();
+//
+//     let list = file
+//         .lines()
+//         .map(|pairs| {
+//             pairs
+//                 .split(",")
+//                 .map(|pair| {
+//                     pair.split("-").map(|v| v.parse::<u32>().unwrap()).collect::<Vec<_>>()
+//                 })
+//                 .flatten()
+//                 .collect::<Vec<_>>()
+//         })
+//         .filter(|u|{
+//             (u[1] >= u[2] && u[0] <= u[1]) || (u[3] >= u[0]  && u[2] < u[0])
+//         })
+//         .collect::<Vec<_>>();
+//     println!("{:?}", list);
+// }
+
+pub fn pairs_part_two() {
     let file = read_file();
 
-    let list = file
+    let list_two = file
         .lines()
         .map(|pairs| {
             pairs
@@ -19,11 +40,8 @@ pub fn pairs_part_one() {
                 .collect::<Vec<_>>()
         })
         .filter(|u|{
-            (u[0] <= u[2] && u[1] >= u[3]) || (u[2] <= u[0] && u[3] >= u[1])
+            (u[1] >= u[2] && u[0] <= u[2]) || (u[3] >= u[0]  && u[2] <= u[0])
         })
         .count();
-
-    println!("{:?}", list);
-
-
+    println!("{:?}", list_two);
 }
