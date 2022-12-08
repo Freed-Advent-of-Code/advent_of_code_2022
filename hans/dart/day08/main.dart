34e1265c7f04402098bd9dda828ce38388c2d3b9
bluewood-truth <bluewood.truth@gmail.com>
2022-12-09 02:14:39 +0900

    solve puzzle 1

--- hans/dart/day08/main.dart
+++ hans/dart/day08/main.dart
@@ -3,8 +3,62 @@ import '../_lib/read_file.dart';
 
 void main() {
   final input = readFile('input');
+  print(measureTime(() => solutionPuzzle1(input)));
 }
 
 int solutionPuzzle1(String input) {
-  return 0;
+  final grid = parseToGrid(input);
+  final gridRows = grid.length;
+  final gridCols = grid[0].length;
+
+  Set<String> visibleTrees = {};
+
+  for (int row = 0; row < gridRows; row++) {
+    int maxHeight = -1;
+    for (int col = 0; col < gridCols; col++) {
+      int treeHeight = grid[row][col];
+      if (treeHeight > maxHeight) {
+        visibleTrees.add('${row},${col}');
+        maxHeight = treeHeight;
+      }
+    }
+
+    maxHeight = -1;
+    for (int col = gridCols - 1; col >= 0; col--) {
+      int treeHeight = grid[row][col];
+      if (treeHeight > maxHeight) {
+        visibleTrees.add('${row},${col}');
+        maxHeight = treeHeight;
+      }
+    }
+  }
+
+  for (int col = 0; col < gridCols; col++) {
+    int maxHeight = -1;
+    for (int row = 0; row < gridRows; row++) {
+      int treeHeight = grid[row][col];
+      if (treeHeight > maxHeight) {
+        visibleTrees.add('${row},${col}');
+        maxHeight = treeHeight;
+      }
+    }
+
+    maxHeight = -1;
+    for (int row = gridRows - 1; row >= 0; row--) {
+      int treeHeight = grid[row][col];
+      if (treeHeight > maxHeight) {
+        visibleTrees.add('${row},${col}');
+        maxHeight = treeHeight;
+      }
+    }
+  }
+
+  return visibleTrees.length;
+}
+
+List<List<int>> parseToGrid(String input) {
+  return input
+      .split('\n')
+      .map((line) => line.split('').map(int.parse).toList())
+      .toList();
 }
