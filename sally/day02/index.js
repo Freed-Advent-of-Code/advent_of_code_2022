05912a4c2d63547c5694056b1a48e3b50d71537f
Hyeon1445 <76871403+Hyeon1445@users.noreply.github.com>
2022-12-08 23:50:15 +0900

    day02 - part2 (#71)

--- sally/day02/index.js
+++ sally/day02/index.js
@@ -1,6 +1,32 @@
-const asciiSub = (char1,char2) => char1.charCodeAt(0) - char2.charCodeAt(0)
-const getTotalScore1 = () => require("fs").readFileSync("sally/day02/input.txt").toString().split("\n").map((game) => asciiSub(game[0], 'A') === asciiSub(game[2], 'X') ? (asciiSub(game[2], 'X') + 1) + 3 : (asciiSub(game[0], 'A') + 1) % 3 === asciiSub(game[2], 'X') ? (asciiSub(game[2], 'X') + 1) + 6 : asciiSub(game[2], 'X') + 1).reduce((sum, score) => sum + score)
-const getTotalScore2 = () => 'TODO'
-
-console.log(getTotalScore1())
-console.log(getTotalScore2())
+const score = {
+  win: 6,
+  draw: 3,
+  lose: 0,
+};
+const inputs = require("fs")
+  .readFileSync("sally/day02/inputs.txt")
+  .toString()
+  .split("\n");
+const subAscii = (char1, char2) => char1.charCodeAt(0) - char2.charCodeAt(0);
+const getScore1 = (opp, me) => {
+  if (opp === me) return me + 1 + score.draw;
+  if ((opp + 1) % 3 === me) return me + 1 + score.win;
+  return me + 1 + score.lose;
+};
+const getScore2 = (opp, result) => {
+  if (result === score.win) return score.win + ((opp + 1) % 3) + 1;
+  if (result === score.draw) return score.draw + opp + 1;
+  return ((opp + 2) % 3) + 1;
+};
+const getTotalScore1 = () =>
+  inputs
+    .map((game) => getScore1(subAscii(game[0], "A"), subAscii(game[2], "X")))
+    .reduce((sum, score) => sum + score);
+const getTotalScore2 = () =>
+  inputs
+    .map((game) =>
+      getScore2(subAscii(game[0], "A"), subAscii(game[2], "X") * 3)
+    )
+    .reduce((sum, score) => sum + score);
+console.log(getTotalScore1());
+console.log(getTotalScore2());
