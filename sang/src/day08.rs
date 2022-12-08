632765383417d3172378fbaf67c1276831fe1eb5
Wonsang <temporaryaion1@hotmail.com>
2022-12-08 23:39:18 +0900

    solve day 8 part 2

--- sang/src/day08.rs
+++ sang/src/day08.rs
@@ -6,6 +6,9 @@ pub async fn solve() {
 
     let part1 = helper::time_function(|| get_part_1(&grid));
     println!("part 1: {}", part1);
+    
+    let part2 = helper::time_function(|| get_part_2(&grid));
+    println!("part 2: {}", part2);
 }
 
 fn process_input(input: &String) -> Vec<Vec<i32>> {
@@ -61,12 +64,83 @@ fn get_part_1(grid: &Vec<Vec<i32>>) -> usize {
     result + 2 * rows + 2 * cols - 4
 }
 
+fn get_part_2(grid: &Vec<Vec<i32>>) -> usize {
+    let (rows, cols) = (grid.len(), grid[0].len());
+    let mut result = 0;
+    let mut history = vec![vec![(0, 0, 0, 0); cols]; rows];
+    for row in 1..rows - 1 {
+        for col in 1..cols - 1 {
+            let (mut top, mut left, mut bottom, mut right) = (0, 0, 0, 0);
+            
+            let mut vision = row - 1;
+            let mut isEnd: bool = false;
+            while grid[vision][col] < grid[row][col] {
+                top += 1;
+                if vision == 0 {
+                    isEnd = true;
+                    break;
+                }
+                vision -= 1;
+            }
+            if !isEnd {
+                top += 1;
+            }
+
+            vision = row + 1;
+            while vision < rows && grid[vision][col] < grid[row][col] {
+                vision += 1;
+                bottom += 1;
+            }
+            if vision < rows {
+                bottom += 1;
+            }
+
+            isEnd = false;
+            vision = col - 1;
+            while grid[row][vision] < grid[row][col] {
+                left += 1;
+                if vision == 0 {
+                    isEnd = true;
+                    break;
+                }
+                vision -= 1;
+            }
+            if !isEnd {
+                left += 1;
+            }
+
+            vision = col + 1;
+            while vision < cols && grid[row][vision] < grid[row][col] {
+                vision += 1;
+                right += 1;
+            }
+            if vision < cols {
+                right += 1;
+            }
+
+            if top * left * bottom * right > result {
+                result = top * left * bottom * right;
+                history[row][col] = (top, left, bottom, right);
+            }
+        }
+    }
+    result
+}
+
 #[test]
 fn test_get_part_1() {
     let grid = match std::fs::read_to_string("input/day8-test.txt") {
         Ok(content) => process_input(&content),
         Err(_) => panic!("invalid day8-test.txt"),
     };
-    println!("{:?}", grid);
     assert_eq!(get_part_1(&grid), 21);
+}
+
+#[test]
+fn test_get_part_2() {
+    let grid = match std::fs::read_to_string("input/day8-test.txt") {
+        Ok(content) => process_input(&content),
+        Err(_) => panic!("invalid day8-test.txt"),
+    };
+    assert_eq!(get_part_2(&grid), 8);
 }
