54440c63b3d4c3f6a0898952f7611668f97f333b
xncbf <xncbf12@gmail.com>
2022-12-09 20:39:25 +0900

    day09 part1

--- francis/day09/main.py
+++ francis/day09/main.py
@@ -1,10 +1,47 @@
 
 def part_one():
-    visited = []
     data = [x.strip().split(" ") for x in open('input.txt', 'r').readlines()]
+    h = (0, 0)
+    t = (0, 0)
+    # previous_t = (0, 0)
+    visited = [t]
     for e in data:
-        pass
-    return
+        for i in range(int(e[1])):
+            if e[0] == "R":
+                h = (h[0], h[1] + 1)
+            elif e[0] == "L":
+                h = (h[0], h[1] - 1)
+            elif e[0] == "U":
+                h = (h[0] + 1, h[1])
+            elif e[0] == "D":
+                h = (h[0] - 1, h[1])
+            if abs(h[0] - t[0]) > 1:
+                if h[0] > t[0]:
+                    t = (t[0] + 1, t[1])
+                elif h[0] < t[0]:
+                    t = (t[0] - 1, t[1])
+
+                if abs(h[1] - t[1]) > 0:
+                    if h[1] > t[1]:
+                        t = (t[0], t[1] + 1)
+                    elif h[1] < t[1]:
+                        t = (t[0], t[1] - 1)
+            elif abs(h[1] - t[1]) > 1:
+                if h[1] > t[1]:
+                    t = (t[0], t[1] + 1)
+                elif h[1] < t[1]:
+                    t = (t[0], t[1] - 1)
+
+                if abs(h[0] - t[0]) > 0:
+                    if h[0] > t[0]:
+                        t = (t[0] + 1, t[1])
+                    elif h[0] < t[0]:
+                        t = (t[0] - 1, t[1])
+
+            else:
+                continue
+            visited.append(t)
+    return len(set(visited))
 
 print(part_one())
 
