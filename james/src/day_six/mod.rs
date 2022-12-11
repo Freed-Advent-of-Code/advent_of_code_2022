c22d250fc059f1690ee60c1e8fa7782b7a9c3629
Dongjoon Min <75886558+djmin43@users.noreply.github.com>
2022-12-11 16:07:48 +0900

    James/day6 pt2 (#88)
    * set up pt 2
    
    * day 6, solve part two

--- james/src/day_six/mod.rs
+++ james/src/day_six/mod.rs
@@ -3,11 +3,8 @@ use itertools::Itertools;
 
 
 pub fn turning_trouble_pt1 () -> String {
-
     let mut answer = String::new();
-
     let f = parse_input();
-
     for i in 0..f.len() - 3 {
         let l = [f[i], f[i+1], f[i+2], f[i+3]]
             .into_iter()
@@ -17,6 +14,31 @@ pub fn turning_trouble_pt1 () -> String {
             println!("the unique index is: {:?}", l);
             println!("first marker after character {}", i + 4);
             answer = (i+4).to_string();
+            break
+        }
+    }
+    answer
+}
+pub fn turning_trouble_pt2 () -> String {
+    let mut answer = String::new();
+    let f = parse_input();
+    for i in 0..f.len() - 13 {
+        let mut l = Vec::new();
+        for ii in i..i+14 {
+            l.push(f[ii]);
+        }
+        let t = l
+            .clone()
+            .into_iter()
+            .unique()
+            .collect::<Vec<_>>();
+
+        if t.len() == 14 {
+            println!("index is: {}", i);
+            println!("vector is : {:?}", t);
+            println!("first marker after character {}", i + 14);
+            answer = (i+14).to_string();
+
             break
         }
     }
--- james/src/main.rs
+++ james/src/main.rs
@@ -2,11 +2,14 @@
 mod day_six;
 
 use day_six::turning_trouble_pt1;
+use day_six::turning_trouble_pt2;
 
 fn main() {
 
-    let answer = turning_trouble_pt1();
-    println!("{}", answer);
+    let answer_pt1 = turning_trouble_pt1();
+    let answer_pt2 = turning_trouble_pt2();
+    println!("part one answer is: {}", answer_pt1);
+    println!("part two answer is: {}", answer_pt2);
 
 }
 
