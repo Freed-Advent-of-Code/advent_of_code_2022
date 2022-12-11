6d4c05ccf5c9122c528098deeaf89495e9a12008
Dongjoon Min <75886558+djmin43@users.noreply.github.com>
2022-12-11 11:35:40 +0900

    solve day 5 part 2 (#84)

--- james/src/day_five/mod.rs
+++ james/src/day_five/mod.rs
@@ -40,3 +40,27 @@ pub fn supply_stacks_pt1 () {
         .collect::<Vec<_>>();
     println!("{:?}", stack_list);
 }
+
+pub fn supply_stacks_pt2 () {
+    let mut stack_list = get_stacks();
+    let file = fs::read_to_string("./src/day_five/input.txt").unwrap();
+    let instructions = file
+        .split("\n\n")
+        .nth(1)
+        .unwrap()
+        .lines()
+        .map(|instruction| {
+            let mut instruction_split = instruction.split_whitespace();
+            let move_size = instruction_split.clone().nth(1).unwrap().parse::<usize>().unwrap();
+            let from_container_index = instruction_split.clone().nth(3).unwrap().parse::<usize>().unwrap();
+            let to_container_index = instruction_split.clone().nth(5).unwrap().parse::<usize>().unwrap();
+            let from_container_len = stack_list[from_container_index - 1].len();
+            let mut drained = stack_list[from_container_index - 1]
+                .drain((from_container_len - move_size)..)
+                .collect::<Vec<_>>();
+            stack_list[to_container_index - 1].append(&mut drained);
+        })
+        .collect::<Vec<_>>();
+    println!("{:?}", stack_list);
+    println!("{:?}", instructions);
+}
--- james/src/main.rs
+++ james/src/main.rs
@@ -1,10 +1,11 @@
 
 mod day_five;
 
-use day_five::supply_stacks_pt1;
+// use day_five::supply_stacks_pt1;
+use day_five::supply_stacks_pt2;
 
 fn main() {
-    supply_stacks_pt1();
+    supply_stacks_pt2();
 
 }
 
