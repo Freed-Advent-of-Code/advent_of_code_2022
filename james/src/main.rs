fec6e5cba95c1cb258cf58f3ed2da1416c150794
jay <dj.min43@gmail.com>
2022-12-12 07:48:30 +0900

    set up day 7

--- james/src/main.rs
+++ james/src/main.rs
@@ -1,16 +1,9 @@
 
-mod day_six;
+mod day_seven;
 
-use day_six::turning_trouble_pt1;
-use day_six::turning_trouble_pt2;
 
 fn main() {
-
-    let answer_pt1 = turning_trouble_pt1();
-    let answer_pt2 = turning_trouble_pt2();
-    println!("part one answer is: {}", answer_pt1);
-    println!("part two answer is: {}", answer_pt2);
-
+   day_seven::no_space_left_on_device_pt1();
 }
 
 
