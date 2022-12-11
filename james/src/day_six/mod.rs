96ba88b858566a75350a965a8dd9e5d73d62e48b
jay <dj.min43@gmail.com>
2022-12-11 15:48:49 +0900

    set up pt 2

--- james/src/day_six/mod.rs
+++ james/src/day_six/mod.rs
@@ -3,11 +3,25 @@ use itertools::Itertools;
 
 
 pub fn turning_trouble_pt1 () -> String {
-
     let mut answer = String::new();
-
     let f = parse_input();
-
+    for i in 0..f.len() - 3 {
+        let l = [f[i], f[i+1], f[i+2], f[i+3]]
+            .into_iter()
+            .unique()
+            .collect::<Vec<_>>();
+        if l.len() == 4 {
+            println!("the unique index is: {:?}", l);
+            println!("first marker after character {}", i + 4);
+            answer = (i+4).to_string();
+            break
+        }
+    }
+    answer
+}
+pub fn turning_trouble_pt () -> String {
+    let mut answer = String::new();
+    let f = parse_input();
     for i in 0..f.len() - 3 {
         let l = [f[i], f[i+1], f[i+2], f[i+3]]
             .into_iter()
