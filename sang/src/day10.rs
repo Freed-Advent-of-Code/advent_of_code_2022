f882a73ea991bdaaad8c672bbd71d91f8f74ddff
Wonsang <temporaryaion1@hotmail.com>
2022-12-13 00:57:11 +0900

    solve day 10 part 2

--- sang/src/day10.rs
+++ sang/src/day10.rs
@@ -2,8 +2,17 @@ use crate::helper;
 
 pub async fn solve() {
     let input = helper::get_input(10).await;
-    let part1 = helper::time_function(||get_part_1(&input));
+    let part1 = helper::time_function(|| get_part_1(&input));
     println!("part 1: {}", part1);
+
+    let part2 = helper::time_function(|| get_part_2(&input));
+    println!("part 2: \n{}", part2);
+    println!("===");
+
+    match std::fs::read_to_string("input/day10-test.txt") {
+        Ok(content) => println!("{}", get_part_2(&content)),
+        Err(_) => panic!("invalid day10-test.txt"),
+    };
 }
 
 fn get_part_1(input: &str) -> i32 {
@@ -41,6 +50,47 @@ fn get_part_1(input: &str) -> i32 {
     total_strength
 }
 
+fn get_part_2(input: &str) -> String {
+    let mut cycle: i32 = 0;
+    let mut position: i32 = 1;
+    let mut final_string = String::new();
+
+    for line in input.lines() {
+        let words: Vec<&str> = line.split_whitespace().collect();
+        let char = if (cycle % 40 - position).abs() <= 1 {
+            '#'
+        } else {
+            '.'
+        };
+
+        if cycle % 40 == 0 && cycle > 1 {
+            final_string.push('\n');
+        }
+        final_string.push(char);
+        cycle += 1;
+
+        match words[0] {
+            "noop" => {}
+            "addx" => {
+                if cycle % 40 == 0 && cycle > 1 {
+                    final_string.push('\n');
+                }
+                let char = if (cycle % 40 - position).abs() <= 1 {
+                    '#'
+                } else {
+                    '.'
+                };
+                final_string.push(char);
+                let addx = words[1].parse::<i32>().unwrap();
+                position += addx;
+                cycle += 1;
+            }
+            _ => panic!("invalid instruction"),
+        }
+    }
+    final_string
+}
+
 #[test]
 fn test_get_part_1() {
     let result = match std::fs::read_to_string("input/day10-test.txt") {
@@ -49,4 +99,16 @@ fn test_get_part_1() {
     };
 
     assert_eq!(result, 13140);
+}
+
+#[test]
+fn test_get_part_2() {
+    let result = match std::fs::read_to_string("input/day10-test.txt") {
+        Ok(content) => get_part_2(&content),
+        Err(_) => panic!("invalid day10-test.txt"),
+    };
+    match std::fs::read_to_string("input/day10-test-output.txt") {
+        Ok(expected) => assert_eq!(result, expected),
+        Err(_) => panic!("invalid day10-test-output.txt"),
+    }
 }
