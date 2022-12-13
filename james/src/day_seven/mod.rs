accc34d4443be953fcdf52dd9d5b1d4d442a62b2
Jay Min <dj.min43@gmail.com>
2022-12-14 06:57:16 +0900

    day 7 wip

--- james/src/day_seven/mod.rs
+++ james/src/day_seven/mod.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 struct File {
     name: String,
-    size: uint32
+    size: i32
 }
 
 struct Dir {
@@ -11,9 +11,21 @@ struct Dir {
 }
 
 pub fn no_space_left_on_device_pt1 () {
-    let f = read_input();
-    println!("{}", f);
 
+    let mut current_dir = Dir {
+        name: String::from(""),
+        files: vec![]
+    };
+
+    let f = read_input()
+        .lines()
+        .map(|line| {
+            let parsed_line = line.split_whitespace().nth(1);
+            println!("{:?}", parsed_line);
+
+        })
+        .collect::<Vec<_>>();
+    println!("{:?}", f);
 }
 
 fn read_input () -> String {
