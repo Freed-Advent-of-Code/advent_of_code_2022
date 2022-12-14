ccc03e5da32f8b18c8d223b7f71046d8d9058ddc
JWSong <thdwjddn123@gmail.com>
2022-12-15 01:25:01 +0900

    Ben/day eight/go (#102)
    * solved part1
    
    * day eight part2 resolved
    
    Co-authored-by: Ben Song <ben.song@freedgrouptech.com>

---
+++ ben/day_eight/go/main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"os"
+	"testing"
+)
+
+func TestPart1(t *testing.T) {
+	f, err := os.Open("test_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	counter := NewTreeCounter(f)
+	got := counter.count()
+	if counter.count() != 21 {
+		t.Errorf("got %d wanted %d", got, 21)
+	}
+}
+
+func TestPart2(t *testing.T) {
+	f, err := os.Open("test_input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	counter := NewTreeCounter(f)
+	got := counter.count()
+	if counter.getHighestScores() != 8 {
+		t.Errorf("got %d wanted %d", got, 8)
+	}
+}
