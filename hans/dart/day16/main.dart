d4a8023931c254912cc0cca914fcb7d87a016ba4
bluewood-truth <bluewood.truth@gmail.com>
2022-12-16 23:13:20 +0900

    setup day16

---
+++ hans/dart/day16/main.dart
@@ -0,0 +1,13 @@
+import '../_lib/measure_time.dart';
+import '../_lib/read_file.dart';
+
+void main() {
+  final input = readFile('input');
+  print(measureTime(() => solution1(input)));
+}
+
+int solution1(String input) {
+  final result = -1;
+
+  return result;
+}
