3cd4da50eb4065f1c58f18d1a34a4c843e447474
Wonsang <temporaryaion1@hotmail.com>
2022-12-17 21:39:56 +0900

    solve day 15 part 2 with brute force

--- sang/src/day15.rs
+++ sang/src/day15.rs
@@ -2,11 +2,15 @@ use crate::helper;
 use std::collections::{HashMap, HashSet};
 
 const destination_y: i64 = 2000000;
+const max_coordinate: i64 = 4000000;
 
 pub async fn solve() {
     let input = helper::get_input(15).await;
     let part1 = helper::time_function(|| get_part_1(&input, destination_y));
     println!("part 1: {}", part1);
+
+    let part2 = helper::time_function(|| get_part_2(&input, max_coordinate as usize));
+    println!("part 2: {}", part2);
 }
 
 #[derive(Debug)]
@@ -43,12 +47,31 @@ fn process_input(input: &str) -> Data {
 }
 
 fn get_part_1(input: &str, target_y: i64) -> i64 {
-    let mut data = process_input(input);
-    let mut intervals: Vec<(i64, i64)> = Vec::new();
+    let data = process_input(input);
+    let stack = get_intervals(&data, target_y as usize);
+
+    let mut count = stack.iter().fold(0, |acc, cur| acc + (cur.1 - cur.0 + 1));
+
+    let occupied_in_y = data
+        .sensors
+        .iter()
+        .filter(|(sx, sy)| *sy == target_y)
+        .count() as i64
+        + data
+            .beacons
+            .iter()
+            .filter(|(bx, by)| *by == target_y)
+            .count() as i64;
+
+    count - occupied_in_y
+}
+
+fn get_intervals(data: &Data, target_y: usize) -> Vec<(i64, i64)> {
+    let mut intervals = vec![];
 
     for (sx, sy) in &data.sensors {
         let dist = data.closest.get(&(*sx, *sy)).unwrap();
-        let vertical = (target_y - *sy).abs();
+        let vertical = (target_y as i64 - *sy).abs();
 
         if vertical > *dist {
             continue;
@@ -73,20 +96,23 @@ fn get_part_1(input: &str, target_y: i64) -> i64 {
         }
     }
 
-    let mut count = stack.iter().fold(0, |acc, cur| acc + (cur.1 - cur.0 + 1));
+    stack
+}
 
-    let occupied_in_y = data
-        .sensors
-        .iter()
-        .filter(|(sx, sy)| *sy == target_y)
-        .count() as i64
-        + data
-            .beacons
-            .iter()
-            .filter(|(bx, by)| *by == target_y)
-            .count() as i64;
+fn get_part_2(input: &str, max_coord: usize) -> i64 {
+    let data = &process_input(input);
 
-    count - occupied_in_y
+    for y in 0..max_coord + 1 {
+        let mut intervals = get_intervals(&data, y);
+        if intervals.len() == 2 {
+            return y as i64 + (intervals[0].1 + 1) * max_coordinate;
+        }
+    }
+    0
+}
+
+fn get_dist((x1, y1): (i64, i64), (x2, y2): (i64, i64)) -> i64 {
+    (x1 - x2).abs() + (y1 - y2).abs()
 }
 
 #[test]
@@ -97,4 +123,14 @@ fn test_get_part_1() {
     };
 
     assert_eq!(result, 26);
+}
+
+#[test]
+fn test_get_part_2() {
+    let result = match std::fs::read_to_string("input/day15-test.txt") {
+        Ok(content) => get_part_2(&content, 20),
+        Err(_) => panic!("invalid day15-test.txt"),
+    };
+
+    assert_eq!(result, 56000011);
 }
